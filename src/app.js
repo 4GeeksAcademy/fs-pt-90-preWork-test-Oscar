b188a00c7bee9ce0c3f34eaab3b15f7da03dfcbb
Galifrey21 <oscarparrapalomino@gmail.com>
2024-10-25 18:01:26 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,39 +7,39 @@ import "./style.css";
 <------------------------- INICIO SELECCION SIMPLE -------------------------->
 Pregunta 1: ✅
 ¿Cuál es la etiqueta correcta para insertar una imagen en HTML?
-a) <img src="ruta"> 
+a) <img src="ruta"> ✅
 b) <image src="ruta">
 
 Pregunta 2: ✅
 ¿Qué etiqueta HTML se utiliza para crear un enlace?
-a) <a href="url">Texto</a> 
+a) <a href="url">Texto</a> ✅
 b) <link href="url">Texto</link>
 
 Pregunta 3: ✅
 ¿Cuál de los siguientes selectores selecciona un elemento por su ID?
-a) #elemento 
+a) #elemento ✅
 b) .elemento
 
 Pregunta 4: ✅
 ¿Cuál es la propiedad CSS que se utiliza para cambiar el color del texto?
-a) color 
+a) color ✅
 b) text-color 
 
 Pregunta 5: ✅
 ¿Cuál propiedad se utiliza para convertir un contenedor en un contenedor flex?
-a) display: flex; 
+a) display: flex; ✅
 b) flex: container
 
 Pregunta 6: ✅
 ¿Qué valor de flex-direction coloca los elementos en una columna vertical?
-a) column 
+a) column ✅
 b) vertical
 
 Pregunta 7: ✅
 ¿Qué diferencia hay entre los valores space-between y space-around para la propiedad justify-content en un contenedor flex?
 
 a) space-between distribuye los elementos con espacios iguales entre ellos, dejando sin espacio los bordes del contenedor, 
-   mientras que space-around agrega espacio igual entre los elementos y en los bordes del contenedor.
+   mientras que space-around agrega espacio igual entre los elementos y en los bordes del contenedor.✅
 
 b) space-between coloca los elementos con espacio igual entre ellos, mientras que space-around distribuye el espacio uniformemente 
    tanto entre los elementos como en los bordes
@@ -53,7 +53,7 @@ let numeroConvertido = parseInt(numero);
 Cual es el resultado de ejecutar un typeof numeroConvertido
 
 a) "string"
-b) "number"
+b) "number"✅
 c) NaN
 d) "boolean"
 
@@ -65,7 +65,7 @@ Que sucede si ejecutamos frutas.push("pera");
 
 a) Se muestra un error porque el arreglo no puede modificarse.
 b) Se agrega "pera" al inicio del arreglo.
-c) Se agrega "pera" al final del arreglo.
+c) Se agrega "pera" al final del arreglo.✅
 d) Se elimina "pera" del arreglo.
 
 Pregunta 10: ✅
@@ -73,7 +73,7 @@ si tenemos:
 const frutas = ["manzana", "banana", "naranja"];
 Cual seria el resultado de hacer frutas.pop();
 
-a) ["manzana", "banana"]
+a) ["manzana", "banana"]✅
 b) ["manzana", "naranja"]
 c) ["banana", "naranja"]
 d) ["manzana", "banana", "naranja"]
@@ -87,7 +87,7 @@ Y ejecutamos saludo.toLowerCase()
 Cual sera el resultado?
 a) "HOLA mundo"
 b) "Hola mundo"
-c) "hola mundo"
+c) "hola mundo"✅
 d) undefined
 
 Pregunta 12: ✅
@@ -110,7 +110,7 @@ quePuedeHacer();
 
 opciones:
 a) Devolvera "No puedes viajar".
-b) Devolvera "Puedes viajar".
+b) Devolvera "Puedes viajar".✅
 c) Devolvera un error de sintaxis.
 d) No Devolvera nada.
 
@@ -133,6 +133,16 @@ window.onload = function() {
   b) Elimina el primer elemento.
   c) Reemplaza "naranja" con "kiwi". */
 
+  let frutas = ["manzana", "banana", "naranja", "piña"];
+
+  frutas.push("mango");
+  console.log(frutas);
+
+  frutas.shift();
+  console.log(frutas);
+
+  frutas.splice(1, 1, "kiwi");
+  console.log(frutas);
   //write your code here
 
   // Pregunta 14:
@@ -142,7 +152,6 @@ window.onload = function() {
   let numeros = [5, 3, 8, 4, 2];
   function bubbleSort(arr) {
     //write your code here
-    return;
   }
 
   console.log(bubbleSort(numeros)); // [2, 3, 4, 5, 8]
